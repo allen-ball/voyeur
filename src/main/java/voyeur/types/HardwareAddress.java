ba8087fa279f834370c35bec195eaa5fdb070c2c
Allen D. Ball <ball@hcf.dev>
2019-05-05 19:37:27 +0000

    Display network interface MAC address.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/voyeur/trunk@4127 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/voyeur/types/HardwareAddress.java
@@ -0,0 +1,54 @@
+/*
+ * $Id$
+ *
+ * Copyright 2019 Allen D. Ball.  All rights reserved.
+ */
+package voyeur.types;
+
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+/**
+ * Hardware Address
+ *
+ * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
+ * @version $Revision$
+ */
+public class HardwareAddress {
+    private final ArrayList<Byte> list = new ArrayList<>();
+
+    /**
+     * {@link NetworkInterface} constructor.
+     *
+     * @param   netif           The {@link NetworkInterface}.
+     */
+    public HardwareAddress(NetworkInterface netif) throws SocketException {
+        this(netif.getHardwareAddress());
+    }
+
+    /**
+     * {@code byte} array constructor.
+     *
+     * @param   bytes           The {@code byte} array representing the
+     *                          hardware address.
+     */
+    public HardwareAddress(byte[] bytes) {
+        if (bytes != null) {
+            for (byte b : bytes) {
+                list.add(b);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String string =
+            list.stream()
+            .map(t -> String.format("%02x", t))
+            .collect(Collectors.joining(":"));
+
+        return string;
+    }
+}
