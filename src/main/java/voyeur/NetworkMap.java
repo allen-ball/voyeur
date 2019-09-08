3afa030d10c608fafc857d338e7d03d9715af81a
Allen D. Ball <ball@hcf.dev>
2019-09-08 17:13:51 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/voyeur/trunk@4553 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/voyeur/NetworkMap.java
@@ -0,0 +1,48 @@
+/*
+ * $Id$
+ *
+ * Copyright 2019 Allen D. Ball.  All rights reserved.
+ */
+package voyeur;
+
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.util.Comparator;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+/**
+ * {@link java.util.Map} implementation mapping host ({@link InetAddress})
+ * to last response.
+ *
+ * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
+ * @version $Revision$
+ */
+public class NetworkMap extends ConcurrentSkipListMap<InetAddress,Long> {
+    private static final long serialVersionUID = -3303365313265425744L;
+
+    private static final Comparator<InetAddress> COMPARATOR =
+        Comparator
+        .<InetAddress>comparingInt(t -> t.getAddress().length)
+        .thenComparingInt(t -> t.isLoopbackAddress() ? -1 : 1)
+        .thenComparing(t -> new BigInteger(1, t.getAddress()));
+
+    /**
+     * Sole constructor.
+     */
+    public NetworkMap() { super(COMPARATOR); }
+
+    /**
+     * See {@link #put(Object,Object)}.
+     *
+     * @param   key             The {@link InetAddress} to add with a last
+     *                          response time of "now."
+     *
+     * @return  {@code true} if {@link.this} map changes; {@code false}
+     *          otherwise.
+     */
+    public boolean add(InetAddress key) {
+        Long value = System.currentTimeMillis();
+
+        return put(key, value) != value;
+    }
+}
