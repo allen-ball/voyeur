8bbcbdcea57d5d003da2334e2af547d3c48b9142
Allen D. Ball <ball@hcf.dev>
2020-01-16 06:42:04 +0000

    Removed javadoc/overview.html and moved comments to top-level package-info.java.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/voyeur/trunk@5037 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/voyeur/package-info.java
+++ src/main/java/voyeur/package-info.java
@@ -1,10 +1,17 @@
 /*
  * $Id$
  *
- * Copyright 2019 Allen D. Ball.  All rights reserved.
+ * Copyright 2019, 2020 Allen D. Ball.  All rights reserved.
  */
 /**
  * Local Area Network Voyeur Application
+ *
+ * {@pom.coordinates}
+ *
+ * <h2>{@link.uri https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/ Spring Boot} Artifacts</h2>
+ *
+ * <h3>application.properties</h3>
+ * {@include /application.properties}
  *
  * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
  */
